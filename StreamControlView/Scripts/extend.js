93d04cd08fbfbebac37e211252a78817309791c3
caseycrocamo <casey.crocamo@gmail.com>
2017-12-06 16:17:17 -0800

    fixed active designation on navbar

--- StreamControlView/Scripts/extend.js
+++ StreamControlView/Scripts/extend.js
@@ -1,9 +1,4 @@
-﻿    //NAV
-    //toggle active on click
-$('.navbar li').click(function () {
-    $('.navbar li').removeClass('active');
-    $(this).addClass('active');
-});
+﻿
     //collapse menu on click
 $(document).on('click', '.navbar-collapse.in', function (e) {
     if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
--- StreamControlView/app/nav/navbar.component.js
+++ StreamControlView/app/nav/navbar.component.js
@@ -1,10 +1,24 @@
 ﻿(function () {
     angular.module("main").component("navbar", {
         controllerAs: "vm",
-        controller: ["$location","authService", function ($location, authService) {
+        controller: ["$location","authService", "$state", function ($location, authService, $state) {
             var vm = this;
             authService.fillAuthData();
             vm.user = authService.authentication;
+
+            vm.$onInit = function () {
+                let stateName = $state.current.name.split('.')[0];
+                if (stateName.indexOf('scoreboard') !== -1) {
+                    stateName = 'scoreboard';
+                }
+                switch (stateName) {
+                    case "scoreboard":
+                        $('#scoreboardsNav').addClass('active');
+                        break;
+                    default:
+                        $('#homeNav').addClass('active');
+                }
+            };
 
             vm.isActive = function (viewLocation) {
                 return viewLocation === $location.path();
@@ -15,10 +29,6 @@
                 location.reload();
             };
 
-            $(".nav li").click(function () {
-                $(".nav li").find(".active").removeClass("active");
-                $(this).parent().addClass("active");
-            });
         }],
         templateUrl: "app/nav/navbar.template.html"
     });
